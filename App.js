b5447eeae94d8e2b0166885ce2ce87760dc1211e
Deden Ramdani <de1103den@gmail.com>
2018-02-11 17:58:48 +1100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from './src/screens/Home';
+import Main from './src/screens/Main';
+export default class App extends Component<Props> {
+  render() {
+    return (
+      
+      <Home />
+       
+    );
+  }
+}
+
